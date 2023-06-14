3e0d9cae04a283285129379c4027522cfdcf325a
Handerson Costa <hac.handerson@gmail.com>
2023-06-14 10:03:13 -0300

    Atividade 9

--- angular/projects/app-de-olho-no-combustivel/src/app/app-routing.module.ts
+++ angular/projects/app-de-olho-no-combustivel/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'formRegister/operation/:title', component: FormRegisterComponent},
+ // {path: '', component: FormComponent},
+ // {path: '', component: DeleteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
