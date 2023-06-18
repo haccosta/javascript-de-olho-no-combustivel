c084d741b8d294f89bb8ed66b79f93914d6fdb42
Handerson Costa <hac.handerson@gmail.com>
2023-06-18 20:36:05 -0300

    Correcao das Validacoes

--- angular/projects/app-de-olho-no-combustivel/src/app/components/criar-abastecimento/criar-abastecimento.component.ts
+++ angular/projects/app-de-olho-no-combustivel/src/app/components/criar-abastecimento/criar-abastecimento.component.ts
@@ -6,19 +6,16 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-criar-abastecimento',
   templateUrl: './criar-abastecimento.component.html',
-  styleUrls: ['./criar-abastecimento.component.css']
+  styleUrls: ['./criar-abastecimento.component.css'],
 })
 export class CriarAbastecimentoComponent {
-
   formulario!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private service : RegistroDeAbastecimentoService,
-    private router: Router,
-    ){
-
-  }
+    private service: RegistroDeAbastecimentoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
@@ -29,26 +26,29 @@ export class CriarAbastecimentoComponent {
           Validators.pattern(/(.|\s)*\S(.|\s)*/),
         ]),
       ],
-      quilometragem: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(1),
-    ])],
-    litrosAbastecidos: ['', Validators.compose([
-      Validators.required,
-      Validators.minLength(1),
-  ])],
+      quilometragem: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(1)]),
+      ],
+      litrosAbastecidos: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(1)]),
+      ],
       jaAbasteceuNessePosto: [false],
       trocouCombustivel: [false],
       tipoCombustivel: [],
     });
-  }
-
-  criarRegistroAbastecimento(){
-    this.service.saveData(new Date().toLocaleTimeString(), this.formulario.value);
-    alert('Cadastrado com Sucesso !')
-    this.router.navigate(['/listarRegistroAbastecimento']);
-
 
   }
 
+  criarRegistroAbastecimento() {
+    if (this.formulario.valid) {
+      this.service.saveData(
+        new Date().toLocaleTimeString(),
+        this.formulario.value
+      );
+      alert('Cadastrado com Sucesso !');
+      this.router.navigate(['/listarRegistroAbastecimento']);
+    }
+  }
 }
